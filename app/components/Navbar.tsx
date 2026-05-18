7a3700ecd4b815cb671eb771e5742bd1b642e217
Ibarracustoms <joaquinibarra81@gmail.com>
2026-05-18 16:54:39 -0700

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,25 +1,11 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav style={styles.nav}>
-      <div style={styles.left}>
-        <Link href="/" style={styles.logoWrap}>
-          <Image
-            src="/Logo.png"
-            alt="Logo"
-            width={40}
-            height={40}
-            style={styles.logo}
-          />
-          <span style={styles.brand}>My Store</span>
-        </Link>
-      </div>
-
-      <div style={styles.right}>
+      <div style={styles.links}>
         <Link href="/" style={styles.link}>Home</Link>
         <Link href="/shop" style={styles.link}>Shop</Link>
         <Link href="/about" style={styles.link}>About</Link>
@@ -30,38 +16,20 @@ export default function Navbar() {
 
 const styles: { [key: string]: React.CSSProperties } = {
   nav: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: "12px 24px",
+    width: "100%",
+    padding: "20px 0",
     borderBottom: "1px solid #eee",
     backgroundColor: "#fff",
   },
-  left: {
+  links: {
     display: "flex",
-    alignItems: "center",
-  },
-  logoWrap: {
-    display: "flex",
-    alignItems: "center",
-    gap: "10px",
-    textDecoration: "none",
-  },
-  logo: {
-    borderRadius: "6px",
-  },
-  brand: {
-    fontSize: "18px",
-    fontWeight: 600,
-    color: "#111",
-  },
-  right: {
-    display: "flex",
-    gap: "20px",
+    justifyContent: "center",
+    gap: "40px",
   },
   link: {
     textDecoration: "none",
-    color: "#333",
+    color: "#111",
     fontSize: "16px",
+    fontWeight: 500,
   },
 };
